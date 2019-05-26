33cce86436294530424edceb439ecb2d2fa28e57
sudhakarchola <38357440+sudhakarchola@users.noreply.github.com>
2019-05-26 16:36:16 +0530

    Update align_dataset_mtcnn.py

--- core/src/align/align_dataset_mtcnn.py
+++ core/src/align/align_dataset_mtcnn.py
@@ -25,8 +25,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from scipy import misc
+#from scipy import misc
 #from scipy.misc import pilutil as misc
+import imageio as misc
 import sys
 import os
 import argparse
